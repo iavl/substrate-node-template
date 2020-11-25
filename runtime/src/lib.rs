d92dfc57e11cd801b0ce8d44890e5ff3a594cb7b
iavl <z6521256@gmail.com>
2020-11-25 21:56:27 +0800

    add poe

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -38,9 +38,6 @@ pub use frame_support::{
 	},
 };
 
-/// Import the template pallet.
-pub use pallet_template;
-
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -261,8 +258,7 @@ impl pallet_sudo::Trait for Runtime {
 	type Call = Call;
 }
 
-/// Configure the template pallet in pallets/template.
-impl pallet_template::Trait for Runtime {
+impl pallet_poe::Trait for Runtime {
 	type Event = Event;
 }
 
@@ -282,7 +278,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Module, Storage},
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
-		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		PoeModule: pallet_poe::{Module, Call, Storage, Event<T>},
 	}
 );
 
