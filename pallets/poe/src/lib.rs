12e64126f7d57d1ccd76d4b58f358df2b2ee5ae4
iavl <z6521256@gmail.com>
2021-01-11 16:58:55 +0800

    add ProofTooLong check and unit test

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -5,7 +5,7 @@
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
 use frame_support::{decl_module, decl_storage, decl_event,
-					ensure, decl_error, dispatch};
+					ensure, decl_error, dispatch, traits::{Get}};
 use frame_system::ensure_signed;
 use sp_std::prelude::*;
 
@@ -19,6 +19,8 @@ mod tests;
 pub trait Trait: frame_system::Trait {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+
+	type MaxClaimLength: Get<u32>;
 }
 
 // The pallet's runtime storage items.
@@ -47,6 +49,7 @@ decl_error! {
 		ProofAlreadyExist,
 		ClaimNotExist,
 		NotClaimOwner,
+		ProofTooLong,
 	}
 }
 
@@ -66,6 +69,8 @@ decl_module! {
 			let sender = ensure_signed(origin)?;
 
 			ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::ProofAlreadyExist);
+
+			ensure!(T::MaxClaimLength::get() >= claim.len() as u32, Error::<T>::ProofTooLong);
 
 			Proofs::<T>::insert(&claim, (sender.clone(), frame_system::Module::<T>::block_number()));
 
--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -24,6 +24,17 @@ fn create_claim_failed_when_claim_already_exist() {
     })
 }
 
+#[test]
+fn create_claim_failed_when_claim_too_long() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1, 2, 3, 4, 5, 6];
+
+        assert_noop!(
+            PoeModule::create_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ProofTooLong
+        );
+    });
+}
 
 #[test]
 fn revoke_claim_works() {
